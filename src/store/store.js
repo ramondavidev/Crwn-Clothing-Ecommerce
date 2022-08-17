69df1f8409e71d8e5702debd38b08a0f5a560480
Ramon <ramondavi@msn.com>
2022-08-17 19:29:06 -0300

    added redux thunk and Spinner component

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,14 @@ import logger from "redux-logger";
 import { configureStore } from '@reduxjs/toolkit'
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user']
+    whitelist: ['cart'] //only persist the cart status (user and categories not persisted)
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -16,7 +17,10 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
     reducer: persistedReducer,
-    middleware: [process.env.NODE_ENV === 'development' && logger].filter(Boolean),
+    middleware: [
+        process.env.NODE_ENV === 'development' && logger, 
+        thunk
+    ].filter(Boolean),
 });
 
 export const persistor = persistStore(store);
